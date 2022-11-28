b94e055bda272882a70edc83a06552d43f97f916
João Souza <56441159+joaomaarinho@users.noreply.github.com>
2022-11-28 00:49:55 -0300

    update

--- src/services/horaextra.ts
+++ src/services/horaextra.ts
@@ -1,6 +1,7 @@
+import { HoraExtra } from "../types";
 import api from "./api";
 
-const create = () => api.post('/horaextra/create')
+const create = () => api.post<HoraExtra>("/horaextra/create");
 
 export const horaextra = {
   create,
